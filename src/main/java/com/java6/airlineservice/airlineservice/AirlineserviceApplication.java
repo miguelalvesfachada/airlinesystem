8853fb37efd4817d439bf00fbb0a14db0dde1dff
carlk <carlkaspar@gmail.com>
2019-11-23 09:48:53 +0200

    This is the base spring boot

---
+++ src/main/java/com/java6/airlineservice/airlineservice/AirlineserviceApplication.java
@@ -0,0 +1,13 @@
+package com.java6.airlineservice.airlineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java6/airlineservice/airlineservice/AirlineserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java6.airlineservice.airlineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
