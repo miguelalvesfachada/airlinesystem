100b770f711fcd490383d7ebfd7b87e72fd38fc3
Miguel Fachada <fachada4@gmail.com>
2019-12-07 15:57:13 +0200

    paginationandstuff

--- src/main/java/com/java6/airlineservice/airlineservice/controllers/AdminController.java
+++ src/main/java/com/java6/airlineservice/airlineservice/controllers/AdminController.java
@@ -1,24 +1,20 @@
 package com.java6.airlineservice.airlineservice.controllers;
 
-import com.java6.airlineservice.airlineservice.models.Airport;
-import com.java6.airlineservice.airlineservice.models.Schedule;
-import com.java6.airlineservice.airlineservice.models.Flight;
-import com.java6.airlineservice.airlineservice.models.Location;
+import com.java6.airlineservice.airlineservice.models.*;
 import com.java6.airlineservice.airlineservice.repository.AirportRepository;
 import com.java6.airlineservice.airlineservice.repository.FlightRepository;
 import com.java6.airlineservice.airlineservice.repository.ScheduleRepository;
 import com.java6.airlineservice.airlineservice.repository.FlightRepository;
 import com.java6.airlineservice.airlineservice.repository.LocationRepository;
-import com.java6.airlineservice.airlineservice.services.AirportServices;
-import com.java6.airlineservice.airlineservice.services.FlightService;
-import com.java6.airlineservice.airlineservice.services.LocationServices;
-import com.java6.airlineservice.airlineservice.services.ScheduleService;
+import com.java6.airlineservice.airlineservice.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.jws.WebParam;
@@ -33,6 +29,8 @@ public class AdminController {
     FlightService flightService;
     @Autowired
     ScheduleService scheduleService;
+    @Autowired
+    BookingService bookingService;
 
 
     @GetMapping("/admin")
@@ -46,16 +44,12 @@ public class AdminController {
     }
 
     @GetMapping("/schedule-admin")
-    public String returnScheduleAdmin(Model model){
+    public String returnScheduleAdmin(Model model) {
         model.addAttribute("flights", flightService.findAllFlights());
         return "schedule-admin";
     }
 
 
-    @GetMapping("/flight-admin")
-    public String returnFlightAdmin() {
-        return "flight-admin";
-    }
 
     @GetMapping("/location-admin")
     public String returnLocationAdmin() {
@@ -70,12 +64,13 @@ public class AdminController {
         return model;
 
     }
-        @PostMapping("/addschedule")
-        public ModelAndView addSchedule(Schedule schedule){
-            ModelAndView model= new ModelAndView();
-            model.addObject("schedule",scheduleService.addSchedule(schedule));
-            model.setViewName("schedule-add-successful");
-            return model;
+
+    @PostMapping("/addschedule")
+    public ModelAndView addSchedule(Schedule schedule) {
+        ModelAndView model = new ModelAndView();
+        model.addObject("schedule", scheduleService.addSchedule(schedule));
+        model.setViewName("schedule-add-successful");
+        return model;
 
 
     }
@@ -83,8 +78,9 @@ public class AdminController {
     @PostMapping("/addflight")
     public ModelAndView addFlight(Flight flight) {
         ModelAndView model = new ModelAndView();
-        model.addObject(flightService.addFlight(flight));
-        model.setViewName("flight-add-successful");
+        flightService.addFlight(flight);
+        model.addObject("flights",flightService.findAllFlights());
+        model.setViewName("flight-admin");
         return model;
     }
 
@@ -97,12 +93,37 @@ public class AdminController {
 
     }
 
-    //TODO display all flights
-    //TODO display all reservations (max 100 per page?)
+    @GetMapping("/flight-admin")
+    public ModelAndView viewAllFlights() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("flights",flightService.findAllFlights());
+        model.setViewName("flight-admin");
+        return model;
+    }
+
+    @GetMapping("/viewReservations")
+    public ModelAndView viewAllReservations(@RequestParam(value = "p", defaultValue = "0") int page) {
+        ModelAndView model = new ModelAndView();
+        Page<Reservation> reservationPage = bookingService.findAllReservations (page);
+        model.addObject("page", reservationPage.getNumber());
+        model.addObject("totalPages", reservationPage.getTotalPages());
+        model.addObject("reservations",reservationPage.getContent());
+        model.setViewName("list-of-all-reservations");
+        return model;
+    }
+
+    @GetMapping("/viewLocations")
+    public ModelAndView viewAllLocations() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("locations",locationServices.findAllLocations ());
+        model.setViewName("list-of-all-locations");
+        return model;
+    }
+
+    //TODO display all reservations (max 100 per page?) --
     //TODO display all locations
     //TODO display all schedules (max 100 per page?)
     //TODO editing functionality for the repositories
     //TODO deleting functionality for the repositories
     //TODO SPRING SECURITY for admin login
-
 }
