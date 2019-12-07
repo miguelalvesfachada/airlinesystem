7152de865a13847df26b352f19acdb78d63fdf11
Thomas Higgins <tplhiggins@fastmail.co.uk>
2019-12-07 09:54:00 +0200

    Merge branch 'master' of https://github.com/miguelalvesfachada/airlinesystem into User
     Conflicts:
    	src/main/java/com/java6/airlineservice/airlineservice/controllers/AdminController.java
    	src/main/resources/templates/flights.html

---
+++ src/main/java/com/java6/airlineservice/airlineservice/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.java6.airlineservice.airlineservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+    public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+            auth.inMemoryAuthentication()
+                    .withUser("Thomas")
+                    .password("admin")
+                    .roles("ADMIN")
+                   .and()
+                    .withUser("Kaspar")
+                    .password("admin")
+                    .roles("ADMIN")
+                    .and()
+                    .withUser("Miguel")
+                    .password("admin")
+                    .roles("ADMIN");
+
+        }
+
+        @Bean
+        public PasswordEncoder getPasswordEncoder(){
+            return NoOpPasswordEncoder.getInstance();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception{
+            http.authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .and().formLogin();
+        }
+    }
+
---
+++ src/main/java/com/java6/airlineservice/airlineservice/security/SpringBootSecurityApplication.java
@@ -0,0 +1,9 @@
+package com.java6.airlineservice.airlineservice.security;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityApplication.class, args);
+    }
+}
