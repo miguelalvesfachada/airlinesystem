e532e5be4ce62e54575a576cdf6dec6f6919018e
Thomas Higgins <tplhiggins@fastmail.co.uk>
2019-12-07 15:52:20 +0200

    Merge branch 'master' of https://github.com/miguelalvesfachada/airlinesystem into User
     Conflicts:
    	src/main/java/com/java6/airlineservice/airlineservice/controllers/AdminController.java
    	src/main/resources/templates/flights.html

--- src/main/java/com/java6/airlineservice/airlineservice/security/SpringBootSecurityApplication.java
+++
@@ -1,9 +0,0 @@
-package com.java6.airlineservice.airlineservice.security;
-
-import org.springframework.boot.SpringApplication;
-
-public class SpringBootSecurityApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityApplication.class, args);
-    }
-}
